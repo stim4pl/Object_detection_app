c775ce327c704587572c0a950ac3995dc86425b1
stim4pl <kacper.jankowski9@gmail.com>
2021-08-18 22:43:53 +0200

    Only 4 objects

--- app/src/main/java/com/example/object_detection_app/MainActivity.kt
+++ app/src/main/java/com/example/object_detection_app/MainActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.util.Size
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
@@ -26,6 +29,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var objectDetector: ObjectDetector
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
 
+    private var detectedObjects: ArrayList<View> = arrayListOf<View>()
+
+    private var detect: Int = 0
+    private var amountDetected: Int = 0
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,22 +43,41 @@ class MainActivity : AppCompatActivity() {
         cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
         cameraProviderFuture.addListener({
-
             val cameraProvider = cameraProviderFuture.get()
             bindPreview(cameraProvider = cameraProvider)
+
 
         }, ContextCompat.getMainExecutor(this))
 
         val localModel = LocalModel.Builder()
-            .setAssetFilePath("mnasnet_1.3_224_1_metadata_1.tflite")
-            .build()
+                .setAssetFilePath("mnasnet_1.3_224_1_metadata_1.tflite")
+                .build()
         val customObjectDetectorOptions = CustomObjectDetectorOptions.Builder(localModel)
-            .setDetectorMode(CustomObjectDetectorOptions.STREAM_MODE)
-            .enableClassification()
-            .setClassificationConfidenceThreshold(0.5f) //Similarity
-            .setMaxPerObjectLabelCount(3)
-            .build()
+                .setDetectorMode(CustomObjectDetectorOptions.STREAM_MODE)
+                .enableClassification()
+                .setClassificationConfidenceThreshold(0.5f) //Similarity
+                .setMaxPerObjectLabelCount(3)
+                .build()
         objectDetector = ObjectDetection.getClient(customObjectDetectorOptions)
+
+        val clearButton = findViewById<Button>(R.id.ClearButton)
+        clearButton.setOnClickListener {
+            for (k in 4 downTo 0 step 1) {
+                binding.parentLayout.removeViewAt(binding.parentLayout.findViewWithTag("child$k"))
+                amountDetected.dec()
+            }
+        }
+
+        val startButton = findViewById<Button>(R.id.startstopButton)
+        startButton.setOnClickListener {
+            if (startButton.text == "Start") {
+                detect = 1
+                startButton.text = "Stop"
+            } else if (startButton.text == "Stop") {
+                detect = 0
+                startButton.text = "Stop"
+            }
+        }
     }
 
     @SuppressLint("UnsafeOptInUsageError")
@@ -59,46 +87,54 @@ class MainActivity : AppCompatActivity() {
         val preview = Preview.Builder().build()
 
         val cameraSelector = CameraSelector.Builder()
-            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-            .build()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
         preview.setSurfaceProvider(binding.previewView.surfaceProvider)
 
 
         val imageAnalysis = ImageAnalysis.Builder()
-            .setTargetResolution(Size(1280, 720))
-            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-            .build()
+                .setTargetResolution(Size(1280, 720))
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
 
-        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this),{imageProxy ->
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), { imageProxy ->
 
             val rotationDegreesValue = imageProxy.imageInfo.rotationDegrees
 
             val image = imageProxy.image
 
-            if(image != null){
+            if (image != null) {
 
                 val processImage = InputImage.fromMediaImage(image, rotationDegreesValue)
 
                 objectDetector
-                    .process(processImage)
-                    .addOnSuccessListener { objects ->
-                        for (i in objects) {
+                        .process(processImage)
+                        .addOnSuccessListener { objects ->
+                            for (i in objects) {
 
-                            if(binding.parentLayout.childCount > 1) binding.parentLayout.removeViewAt(1)
+                                if (amountDetected > 3) {
+                                    for (k in 1..4) {
+                                        binding.parentLayout.removeView(detectedObjects[0])
+                                        detectedObjects.removeAt(0)
+                                        amountDetected -= 1
+                                        Log.d("MainActivity", "Dec")
+                                    }
 
-                            val element = Draw(context = this,
-                                rect = i.boundingBox,
-                                text = i.labels.firstOrNull()?.text ?: "Undefined")
+                                }
 
-                            binding.parentLayout.addView(element)
+                                val element = Draw(context = this,
+                                        rect = i.boundingBox,
+                                        text = i.labels.firstOrNull()?.text ?: "Undefined")
+                                amountDetected += 1
+                                detectedObjects.add(element)
+                                binding.parentLayout.addView(element)
+                            }
+
+                            imageProxy.close()
+                        }.addOnFailureListener {
+                            Log.v("MainActivity", "Error - ${it.message}")
+                            imageProxy.close()
                         }
-
-                        Log.d("MainActivity", "Cos robi")
-                        imageProxy.close()
-                    }.addOnFailureListener {
-                        Log.v("MainActivity", "Error - ${it.message}")
-                        imageProxy.close()
-                    }
             }
         })
 
