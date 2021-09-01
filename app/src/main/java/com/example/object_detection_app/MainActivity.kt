333f6eb5f4107b6583407e988160f50085ff932a
stim4pl <kacper.jankowski9@gmail.com>
2021-09-01 20:41:41 +0200

    Commit

--- app/src/main/java/com/example/object_detection_app/MainActivity.kt
+++ app/src/main/java/com/example/object_detection_app/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.object_detection_app
 
 import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,26 +10,37 @@ import android.util.Size
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.size
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LifecycleOwner
 import com.example.object_detection_app.databinding.ActivityMainBinding
+import com.google.a.b.a.a.a.e
+import com.google.android.gms.tasks.Task
 import com.google.common.util.concurrent.ListenableFuture
+import com.google.mlkit.common.MlKitException
 import com.google.mlkit.common.model.LocalModel
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.objects.DetectedObject
 import com.google.mlkit.vision.objects.ObjectDetection
 import com.google.mlkit.vision.objects.ObjectDetector
 import com.google.mlkit.vision.objects.custom.CustomObjectDetectorOptions
 
 class MainActivity : AppCompatActivity() {
 
+
+    val CAMERA_RQ = 102
+
+
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var objectDetector: ObjectDetector
     private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private lateinit var task: Task<List<DetectedObject>>
 
     private var detectedObjects: ArrayList<View> = arrayListOf<View>()
 
@@ -37,6 +50,9 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        checkForPermission(android.Manifest.permission.CAMERA, "camera", CAMERA_RQ)
+
         setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
@@ -50,32 +66,83 @@ class MainActivity : AppCompatActivity() {
         }, ContextCompat.getMainExecutor(this))
 
         val localModel = LocalModel.Builder()
-                .setAssetFilePath("mnasnet_1.3_224_1_metadata_1.tflite")
+                .setAssetFilePath("mnasnet_0.50_224_1_metadata_1.tflite")
                 .build()
         val customObjectDetectorOptions = CustomObjectDetectorOptions.Builder(localModel)
                 .setDetectorMode(CustomObjectDetectorOptions.STREAM_MODE)
+                .enableMultipleObjects()
                 .enableClassification()
-                .setClassificationConfidenceThreshold(0.5f) //Similarity
-                .setMaxPerObjectLabelCount(3)
+                .setClassificationConfidenceThreshold(0.5F) //Similarity
+                .setMaxPerObjectLabelCount(1)
                 .build()
         objectDetector = ObjectDetection.getClient(customObjectDetectorOptions)
 
         val clearButton = findViewById<Button>(R.id.ClearButton)
         clearButton.setOnClickListener {
-            for (k in 4 downTo 0 step 1) {
-                binding.parentLayout.removeViewAt(binding.parentLayout.findViewWithTag("child$k"))
-                amountDetected.dec()
-            }
+            clearObjects()
         }
 
         val startButton = findViewById<Button>(R.id.startstopButton)
         startButton.setOnClickListener {
-            if (startButton.text == "Start") {
-                detect = 1
-                startButton.text = "Stop"
-            } else if (startButton.text == "Stop") {
-                detect = 0
-                startButton.text = "Stop"
+            when (startButton.text) {
+                "Start" -> {
+                    detect = 1
+                    startButton.text = "Stop"
+                }
+                "Stop" -> {
+                    detect = 0
+                    startButton.text = "Start"
+                }
+            }
+        }
+    }
+
+    private fun checkForPermission(permission: String, name: String, requestCode: Int) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            when {
+                ContextCompat.checkSelfPermission(applicationContext, permission) == PackageManager.PERMISSION_GRANTED -> {
+                    Toast.makeText(applicationContext, "$name permission granted", Toast.LENGTH_SHORT).show()
+                }
+                shouldShowRequestPermissionRationale(permission) -> showDialog(permission, name, requestCode)
+                else -> ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission), requestCode)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        fun innerCheck(name: String) {
+            if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(applicationContext, "$name permission refused", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(applicationContext, "$name permission granted", Toast.LENGTH_SHORT).show()
+            }
+        }
+        when (requestCode) {
+            CAMERA_RQ -> innerCheck("camera")
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun showDialog(permission: String, name: String, requestCode: Int) {
+        val builder = AlertDialog.Builder(this)
+
+        builder.apply {
+            setMessage("Permission to access your $name is required to use this app")
+            setTitle("Permission required")
+            setPositiveButton("OK") { dialog, which ->
+                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission), requestCode)
+            }
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    private fun clearObjects() {
+        if (detectedObjects.isNotEmpty()) {
+            for (item in detectedObjects) {
+                binding.parentLayout.removeView(item)
+                amountDetected -= 1
+                Log.d("MainActivity", "Dec")
             }
         }
     }
@@ -106,35 +173,27 @@ class MainActivity : AppCompatActivity() {
             if (image != null) {
 
                 val processImage = InputImage.fromMediaImage(image, rotationDegreesValue)
-
                 objectDetector
                         .process(processImage)
                         .addOnSuccessListener { objects ->
+                            clearObjects()
                             for (i in objects) {
-
-                                if (amountDetected > 3) {
-                                    for (k in 1..4) {
-                                        binding.parentLayout.removeView(detectedObjects[0])
-                                        detectedObjects.removeAt(0)
-                                        amountDetected -= 1
-                                        Log.d("MainActivity", "Dec")
-                                    }
-
-                                }
-
                                 val element = Draw(context = this,
                                         rect = i.boundingBox,
                                         text = i.labels.firstOrNull()?.text ?: "Undefined")
                                 amountDetected += 1
                                 detectedObjects.add(element)
-                                binding.parentLayout.addView(element)
+
+                                if (detect == 1) binding.parentLayout.addView(element)
                             }
 
                             imageProxy.close()
+
                         }.addOnFailureListener {
-                            Log.v("MainActivity", "Error - ${it.message}")
+                            Log.e("MainActivity", "Error - ${it.message}")
                             imageProxy.close()
                         }
+
             }
         })
 
